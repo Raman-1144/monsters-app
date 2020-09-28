53e41a6eb017ab2ac248d5e9fb4782c42bf6b63e
Ramanpreet <ramanpreet.kaur@destm.com>
2020-09-28 12:32:42 +0530

    Updates

---
+++ precache-manifest.89632bd3c59ff2c0147b1878ba1eef13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14cd6b9737a30e5a45cdd08cd29d7c76",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "c0d5cd0af6b924f6df3d",
+    "url": "/monsters-app/static/css/main.00533002.chunk.css"
+  },
+  {
+    "revision": "4b4d008964b1136230e0",
+    "url": "/monsters-app/static/js/2.e373ee96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.e373ee96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d5cd0af6b924f6df3d",
+    "url": "/monsters-app/static/js/main.ef583244.chunk.js"
+  },
+  {
+    "revision": "37fffd482e28466931d8",
+    "url": "/monsters-app/static/js/runtime-main.aeb0c7f8.js"
+  }
+]);
