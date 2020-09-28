5627c976a4f9ed9cac2de4a978fee3d19ec71a82
Ramanpreet <ramanpreet.kaur@destm.com>
2020-09-28 17:42:06 +0530

    grid updates

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './card-list.styles.css';
 import {Card} from '../card/card.component';
+import Grid from '@material-ui/core/Grid';
 export const CardList = props => {
     console.log(props)
-return <div className='card-list'>
+return <Grid className='card-list' xs={4}>
     {
      props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)
   }
-</div>
+</Grid>
 }
